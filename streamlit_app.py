9e5e23060a57ab8a939bba8d26007a6603599b3b
Beda <vedprakash.satapathy@gmail.com>
2024-04-23 22:52:28 +0530

    gapi added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from google.cloud importaiplatformv1beta1 as gapiclient
+from google.cloud.importaiplatformv1beta1 as gapiclient
 
 # Replace with your Gemini API key
 API_KEY = "AIzaSyBcbUSkMPUYpdu9f8QdH46voXl0z84TQCY"
