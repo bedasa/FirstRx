3210b21ab79889d29a66bc6adeb8cc11133e099b
Chetan Mishra <105204809+chetanc11@users.noreply.github.com>
2024-04-23 23:46:56 +0530

    Update streamlit_app.py to remove betav1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from google.cloud import aiplatformv1beta1 as gapiclient
+from google.cloud import aiplatform as gapiclient
 
 # Replace with your Gemini API key
 API_KEY = "AIzaSyBcbUSkMPUYpdu9f8QdH46voXl0z84TQCY"
